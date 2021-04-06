41ac4a195c0a1fb8d5b290c14bd5e73f4933fa2c
GitHub Action <action@github.com>
2021-04-06 15:20:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/furetur/kotlinstatsextractor/services/MyProjectService.kt
+++ src/main/kotlin/com/github/furetur/kotlinstatsextractor/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.furetur.kotlinstatsextractor.services
 
+import com.github.furetur.kotlinstatsextractor.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
